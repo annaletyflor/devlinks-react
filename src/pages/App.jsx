c67d574a8a990e514c17e3f0158ed63a52f5d208
annaletyflor <annaletyflor@gmail.com>
2023-08-10 17:11:54 -0300

    Finalizando

---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import perfil from "../components/profile/profile"
+import Profile from "../components/profile/profile";
+
+export default function App() {
+    return (
+        <>
+        <Profile></Profile>
+        </>
+    );
+}
